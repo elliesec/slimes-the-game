6d9c526705fbb15079586424862b7087011c29f6
Ellie <62729616+elliesec@users.noreply.github.com>
2020-08-11 01:22:35 +0100

    Renames site map directory

--- src/siteMap.tsx
+++ src/siteMap.tsx
@@ -1,6 +1,6 @@
 import { h, render } from 'preact';
 import './index.scss';
-import { SiteMap } from './siteMap/SiteMap';
+import { SiteMap } from './site-map/SiteMap';
 
 const appRoot = document.getElementById('app-root');
 
