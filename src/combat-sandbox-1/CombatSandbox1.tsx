0e0b4d34d8e01f593a71edd91169f633628b5c76
Ellie <62729616+elliesec@users.noreply.github.com>
2020-08-14 21:14:15 +0100

    Layout updates to the combat sandbox page

--- src/combat-sandbox-1/CombatSandbox1.tsx
+++ src/combat-sandbox-1/CombatSandbox1.tsx
@@ -4,10 +4,16 @@ import { Dispatch } from 'redux';
 import { noop } from '../common/functions';
 import './CombatSandbox1.scss';
 import { ConnectedPlayerConfigPanel } from './components/PlayerConfigPanel/PlayerConfigPanel';
+import { encounter1 } from './encounter1';
+import { Scene } from './enums';
 import { Player } from './Player';
+import { registerEncounter } from './redux/actions/encounter-actions';
+import { setScene } from './redux/actions/game-actions';
 import { setPlayer } from './redux/actions/player-actions';
 import { defaultPlayer } from './redux/reducers/player-reducer';
 import { State } from './redux/store';
+
+const mimic = require('./assets/mimic.jpg');
 
 export interface CombatSandbox1Props {
     player: Player;
@@ -21,6 +27,7 @@ export class CombatSandbox1 extends Component<CombatSandbox1Props> {
 
     public constructor(props: CombatSandbox1Props) {
         super(props);
+        props.load();
     }
 
     public render({ player }: CombatSandbox1Props): VNode {
@@ -35,6 +42,34 @@ export class CombatSandbox1 extends Component<CombatSandbox1Props> {
                 <div className="encounter-panel">
                     <div className="panel-header">
                         <h2>Encounter</h2>
+                    </div>
+                    <div className="encounter-view">
+                        <div className="text">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Aenean consectetur in nunc et
+                                pharetra. Sed maximus lobortis diam sit amet
+                                pharetra. Curabitur condimentum varius
+                                porttitor. Fusce sit amet diam libero. Phasellus
+                                ut vehicula mi, in sollicitudin nunc. In vel
+                                ultricies justo. Quisque dui justo, luctus
+                                posuere vehicula in, hendrerit sit amet ligula.
+                            </p>
+                            <p>
+                                Fusce tempor velit eget urna vehicula, sit amet
+                                mattis nisl convallis. Vestibulum imperdiet
+                                ullamcorper nunc sed vulputate. Morbi ac sapien
+                                rhoncus, egestas arcu at, tincidunt lectus. Nunc
+                                sit amet urna maximus diam pharetra ultricies
+                                sed sit amet nibh. Quisque vulputate metus a
+                                nunc aliquet viverra. Proin quis auctor arcu. Ut
+                                vulputate ante non venenatis rhoncus. Aliquam
+                                sodales sapien eu sagittis luctus.
+                            </p>
+                        </div>
+                        <div className="images">
+                            <img src={mimic} alt="Lewd Pictures" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,7 +86,9 @@ function mapStateToProps(state: State): Partial<CombatSandbox1Props> {
 function mapDispatchToProps(dispatch: Dispatch): Partial<CombatSandbox1Props> {
     return {
         load(): void {
+            dispatch(registerEncounter(encounter1));
             dispatch(setPlayer(defaultPlayer()));
+            dispatch(setScene(Scene.ENCOUNTER_SELECT));
         },
     };
 }
