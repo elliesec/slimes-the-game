8f745eb9ddc83c52f1b422473c4dd37d2b205274
Ellie <62729616+elliesec@users.noreply.github.com>
2020-08-15 02:52:34 +0100

    Removes a spurious console.log

--- src/combat-sandbox-1/redux/reducers/current-scene-reducer.ts
+++ src/combat-sandbox-1/redux/reducers/current-scene-reducer.ts
@@ -7,8 +7,6 @@ const reducers: Record<string, Reducer<Scene>> = {
     [GameActions.SET_SCENE]: setSceneReducer,
 };
 
-console.log(Scene);
-
 export function currentSceneReducer(currentScene = Scene.ENCOUNTER_SELECT, action: Action): Scene {
     const reducer = reducers[action.type];
     return reducer ? reducer(currentScene, action) : currentScene;
