26d4fa521219ed4f39f85335a79ee9a72b59d02a
Ellie <62729616+elliesec@users.noreply.github.com>
2020-12-07 00:50:37 +0000

    Main Menu adjustments

--- src/slimes-the-game/components/views/MainMenuView/MainMenuView.tsx
+++ src/slimes-the-game/components/views/MainMenuView/MainMenuView.tsx
@@ -5,7 +5,7 @@ import { GAME_NAME } from '../../../config/config';
 import { MainMenuButton } from './MainMenuButton';
 import './MainMenuView.scss';
 
-const mainMenuBackground = require('../../../assets/image-placeholder.png');
+const mainMenuBackground = require('./main-menu-bg.png');
 
 export class MainMenuView extends PixiAppView {
     public render(): ReactElement {
@@ -13,12 +13,15 @@ export class MainMenuView extends PixiAppView {
             <div className="AppView MainMenuView" style={this.getBackgroundStyle()}>
                 <div className="menu-backdrop">
                     <h1>{GAME_NAME}</h1>
-                    <ButtonGrid rows={2} columns={1}>
+                    <ButtonGrid rows={3} columns={1}>
                         <MainMenuButton row={0} column={0}>
                             New Game
                         </MainMenuButton>
                         <MainMenuButton row={1} column={0}>
                             Load Game
+                        </MainMenuButton>
+                        <MainMenuButton row={2} column={0}>
+                            Settings
                         </MainMenuButton>
                     </ButtonGrid>
                 </div>
