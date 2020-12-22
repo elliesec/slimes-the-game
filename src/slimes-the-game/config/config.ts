ec56805cf2cd202b1b6cbddf9c60bcf75bee6332
Ellie <62729616+elliesec@users.noreply.github.com>
2020-12-22 21:39:26 +0000

    Removes unneeded consts

--- src/slimes-the-game/config/config.ts
+++ src/slimes-the-game/config/config.ts
@@ -4,7 +4,3 @@ export const GAME_NAME = process.env.GAME_NAME;
 // Asset Config
 export const CHARACTER_ASSET_WIDTH = 1800;
 export const CHARACTER_ASSET_HEIGHT = 1500;
-
-// Screen Config
-export const LOADING_SCREEN_FADE_IN_MS = 1000;
-export const LOADING_SCREEN_FADE_OUT_MS = 1000;
